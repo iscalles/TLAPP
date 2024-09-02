164ce510752421bdf17a87c0b263250c430b08d7
iscalles <is.calles@duocuc.cl>
2024-09-02 09:16:18 -0400

    update home page ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
+import { Router, NavigationExtras, ActivatedRoute } from '@angular/router'; 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
+  usuario: String;
+  niveles:any[]=[
+    {id:1,nivel:"Basica incompleta"},
+    {id:2,nivel:"Basica completa"},
+    {id:3,nivel:"Media incompleta"},
+    {id:4,nivel:"Media completa"},
+    {id:5,nivel:"Superior incompleta"},
+    {id:6,nivel:"Superior completa"}
+  ]
+  data:any={
+    nombre:"",
+    apellido:"",
+    educacion:"",
+    nacimiento:""
+  };
+  constructor(public alertController: AlertController, private activeRoute: ActivatedRoute, private router: Router) {
+    this.activeRoute.queryParams.subscribe(params =>{
+      if(this.router.getCurrentNavigation().extras.state){
+        this.usuario=this.router.getCurrentNavigation().extras.state.user;
+      }
+    });
+  }
 
 }
